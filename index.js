14831c9722801241694939778a4962387376fee7
Samuel Machado <crow.script@gmail.com>
2019-04-04 15:45:31 +0100

    Add some comments

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
+//----String function--Start----
 function grow (name,lastname) {
     console.log( 'Hey '+ name + ' '+lastname );
 
 }
 
 grow('Anny','Smith');
+
+
+//-----End--------------------------
